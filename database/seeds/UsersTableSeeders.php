2e37eeebc096f5ace603c5b120349024cb9b2c56
dujianjun <v@jianwi.cn>
2020-03-10 22:40:05 +0800

    填充用户和话题数据

--- database/seeds/UsersTableSeeders.php
+++ database/seeds/UsersTableSeeders.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
 class UsersTableSeeders extends Seeder
 {
     /**
@@ -12,11 +12,44 @@ class UsersTableSeeders extends Seeder
     public function run()
     {
         //
-        DB::table('users')->insert([
-            'name' => 'dujianjun',
-            'email' => 'v@jianwi.cn',
-            'password' => Hash::make('jianwi.cn'),
-            'email_verified_at' => "2020-3-1 14:00"
-        ]);
+
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+
+        // 生成数据集合
+        $users = factory(User::class)
+            ->times(10)
+            ->make()
+            ->each(function ($user, $index)
+            use ($faker, $avatars)
+            {
+                // 从头像数组中随机取出一个并赋值
+                $user->avatar = $faker->randomElement($avatars);
+            });
+
+        // 让隐藏字段可见，并将数据集合转换为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        User::insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'dujianjun';
+        $user->email = 'v@jianwi.cn';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->password = bcrypt('jianwi.cn');
+        $user->save();
     }
+
 }
