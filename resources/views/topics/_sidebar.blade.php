b031ba2b87daccf0c5a69b526f04f494fbef1b20
dujianjun <v@jianwi.cn>
2020-03-11 16:21:42 +0800

    用户可以创建话题

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,5 +1,8 @@
 <div class="card ">
   <div class="card-body">
-    右边导航栏
+    <a href="{{ route('topics.create') }}" class="btn btn-success btn-block">
+      <span class="fas fa-pencil-alt mr-2"></span>
+      新建帖子
+    </a>
   </div>
 </div>
