f867e7fe661e31d364682d2a27678149ff86ad0f
dujianjun <v@jianwi.cn>
2020-03-12 17:34:30 +0800

    用户可以发表评论

--- resources/views/topics/_reply_box.blade.php
+++ resources/views/topics/_reply_box.blade.php
@@ -1,4 +1,15 @@
-<form action="{{ route('replies.store') }}" method="post">
-  {{ csrf_field() }}
-  {{ method_field('patch') }}
-</form>
+@include('shared._error')
+<div class="reply-box">
+  <form action="{{ route('replies.store') }}" method="post">
+    {{ csrf_field() }}
+    <input type="text" name="topic_id" value="{{ $topic->id }}" hidden id="">
+    <div class="form-group">
+      <textarea name="content" id="" class="form-control" placeholder="分享你的见解"  rows="3"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary btn-sm">
+      <span class="fa fa-share mr-1"></span>
+      回复
+    </button>
+  </form>
+</div>
+<hr>
